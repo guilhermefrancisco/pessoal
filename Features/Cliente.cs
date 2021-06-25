cb430dc4e042f15017ab36a3dbea8eddebfcaa48
Guilherme Francisco <guilherme.francis2017@gmail.com>
2021-06-24 22:11:39 -0300

    initial

---
+++ Features/Cliente.cs
@@ -0,0 +1,17 @@
+﻿namespace Record_Types
+{
+    /* 
+     * É um objeto de valor de dados imutável
+     * A herança funciona aqui também! só não podemos herdar um record de qualquer outra classe não sendo object
+     * Não podemos herdar uma classe de um record.
+     * 
+     * */
+
+    public record Cliente
+    {
+        // init => se estiver numa classe, utilize -> init <- para fazer a propriedade ser imutável, e o restante não será
+        public string Nome { get; init; }
+        public string Sobrenome { get; init; }
+        public string Email { get; init; }
+    }
+}
---
+++ Features/Program.cs
@@ -0,0 +1,27 @@
+﻿using Record_Types;
+using System;
+
+namespace Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {    
+            Cliente cliente = new Cliente { Nome = "Guilherme", Email = "guilherme.francisco@gmail.com.br" };
+
+            // -> Nova forma de cópia com a expressão -> With <-
+            Cliente novoCliente = cliente with { Email = "guilherme@gmail.com.br" };
+
+            // false
+            Console.WriteLine(cliente == novoCliente);
+
+            Cliente outroCliente = novoCliente with { Email = "guilherme.francisco@gmail.com.br" };
+
+            // true
+            Console.WriteLine(cliente == outroCliente);
+
+            Console.ReadLine();
+
+        }
+    }
+}
